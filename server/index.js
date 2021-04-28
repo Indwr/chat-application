d6fb278caca272680d740e03bbd95cde0ce2591c
Inder sein <indersein416@gmail.com>
2021-04-28 17:22:30 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -9,19 +9,21 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+
+const io = require('socket.io').listen(server);
+
 
 app.use(cors());
 app.use(router);
 
-io.on('connect', (socket) => {
+io.on('connection', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
     if(error) return callback(error);
 
     socket.join(user.room);
-
+ 
     socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
